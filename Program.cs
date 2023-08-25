725bb7f8cf534ec30980ab2d6b7c186b66b08b24
sananeo0 <sananeo@code.edu.az>
2023-08-25 11:09:06 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your date (dd | MM | yyyy): ");
+            string input = Console.ReadLine();
+            try
+            {
+                DateTime time = DateTime.ParseExact(input, "dd | MM | yyyy", null);
+                Console.WriteLine("Date: "+time.ToString("dd | MM | yyyy"));
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+         
+        }
+    }
+}
